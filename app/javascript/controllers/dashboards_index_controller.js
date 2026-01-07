28b024daef9caf0b1edfffdf19456c7d424f1f0c
Ryan Murphy <murjax@gmail.com>
2026-01-07 08:06:36 -0500

    Play link and player

--- app/javascript/controllers/dashboards_index_controller.js
+++ app/javascript/controllers/dashboards_index_controller.js
@@ -3,6 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["map"];
 
+  selectedFeeds = [];
+
   async mapTargetConnected() {
     const leafletMap = L.map(this.mapTarget.id).setView([39.8195318, -98.6316774], 5);
 
@@ -41,7 +43,12 @@ export default class extends Controller {
 
   addFeed(audioFeed) {
     if (!!document.querySelector(`[data-feed-id="${audioFeed.id}"]`)) { return; }
+    this.selectedFeeds.push(audioFeed);
+    this.updatePlayUrl();
+    this.addFeedElement(audioFeed);
+  }
 
+  addFeedElement(audioFeed) {
     const selectedFeedsElement = document.querySelector("#selected-feeds");
     const selectedFeedTemplate = document.querySelector("#selected-feed-template");
     const selectedFeedElement = selectedFeedTemplate.cloneNode(true);
@@ -54,6 +61,21 @@ export default class extends Controller {
   }
 
   removeFeed(audioFeedId) {
+    const audioFeed = this.selectedFeeds.find((audioFeed) => audioFeed.id === audioFeedId);
+    const index = this.selectedFeeds.indexOf(audioFeed);
+    this.selectedFeeds.splice(index, 1);
+    this.updatePlayUrl();
+    this.removeFeedElement(audioFeedId);
+  }
+
+  removeFeedElement(audioFeedId) {
     document.querySelector(`[data-feed-id="${audioFeedId}"]`).remove();
+  }
+
+  updatePlayUrl() {
+    const audioFeedIds = this.selectedFeeds.map((audioFeed) => audioFeed.id);
+    const href = `/feed_listeners?audio_feed_ids=${audioFeedIds.join(",")}`;
+    const feedListenerLink = document.querySelector("#feed-listener-link");
+    feedListenerLink.setAttribute("href", href);
   }
 }
